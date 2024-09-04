75dc4167a37440d6d9dcf1fdc7898debf0572dae
Amal <ken940005@gmail.com>
2024-09-05 00:10:02 +0530

    new

--- ui/sub/index/WhyIndustries/WhyIndustries.tsx
+++ ui/sub/index/WhyIndustries/WhyIndustries.tsx
@@ -27,7 +27,7 @@ export default function ExtraCards() {
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
-                <img className="max-w-full mx-auto md:max-w-none h-auto" src="/images/who/img1.png" width={540} height={405} alt="Features 01" />
+                <img className="max-w-full mx-auto md:max-w-none h-auto" src="/images/index/img222.jpeg" width={540} height={405} alt="Features 01" />
               </div>
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
