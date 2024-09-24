1fb359b58254a5967b259bc7d445cbbe310dccb0
AmalRoy55 <163969518+AmalRoy55@users.noreply.github.com>
2024-09-24 17:57:49 +0530

    connection security (#11)

--- src/conifgs/mongoClient.ts
+++ src/conifgs/mongoClient.ts
@@ -1,12 +1,14 @@
 import { MongoClient } from "mongodb";
 
-const url = "mongodb+srv://amalgenfinity:LPshbbL1U0IS6zQm@cluster0.gwygt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const url = process.env.MONGODB_CONNECTION_URI || "";
 const dbName = "ai-summit";
 
 let db: any;
 
 export async function connectToDatabase() {
   try {
+    if(!url || url === "") return console.log("connection url seems empty please check the env");
+    
     if (!db) {
       const client = new MongoClient(url);
       await client.connect();
