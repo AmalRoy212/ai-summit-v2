e1d8e6eefc61a27d4e12590e211b5383737f32c5
AmalRoy55 <amal@ethan-ai.com>
2024-09-29 22:13:35 +0530

    edits

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -6,11 +6,10 @@ import { NextResponse } from "next/server";
 export async function GET() {
   // Optional: You could implement a GET request to retrieve all users or a specific user.
   try {
-    const result = await getData('sample');    
-    return NextResponse.json(
-      { message: "All data collected", data: result },
-      { status: 200 }
-    );
+    const result = await getData('sample');
+    const response = NextResponse.json({ message: "All data collected", data: result }, { status: 200 });
+    response.headers.set('Cache-Control', 's-maxage=0, stale-while-revalidate=0');
+    return response;
   } catch (error) {
     throw new Error('Cannot retrive the sample data at this movement')
   }
