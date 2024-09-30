2d012c05e95d58ea497be15ac8edf8a583be40b2
AmalRoy55 <amal@ethan-ai.com>
2024-09-30 13:00:41 +0530

    new sample api

---
+++ pages/api/sample.ts
@@ -0,0 +1,22 @@
+// pages/api/test.js
+import { connectToDatabase } from '@/src/conifgs/mongoClient';
+import { getData } from '@/src/services/getAllDataService';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req : NextApiRequest, res : NextApiResponse) {
+  await connectToDatabase();
+
+  // Disable caching
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+
+  if (req.method === 'GET') {
+    try {
+      const data = await getData('sample'); // Fetch the latest data from DB
+      res.status(200).json({ success: true, data });
+    } catch (error : any) {
+      res.status(500).json({ success: false, message: error.message });
+    }
+  } else {
+    res.status(405).json({ success: false, message: 'Method Not Allowed' });
+  }
+}
