1f0a9fe4df6c13117b891b6c64fbd9ba8596517e
AmalRoy55 <amal@ethan-ai.com>
2024-10-15 01:26:18 +0530

    nothing

--- ui/sub/index/aboutEvent/Occation.tsx
+++ ui/sub/index/aboutEvent/Occation.tsx
@@ -41,7 +41,7 @@ function Occation() {
           whileInView="show"
           className="relative flex flex-col md:w-1/4 w-full h-[50vh] justify-center items-center p-5 md:mt-[-8rem] mt-[-8rem]"
         >
-          <h1 className="md: absolute md:top-16 mt-[-9rem] text-6xl md:m-[-1rem] text-center font-bold text-slate-200 z-10">
+          <h1 className="md: absolute md:top-16 mt-[-9rem] text-5xl md:m-[-1rem] text-center font-bold text-slate-200 z-10">
             $28.50B
           </h1>
           <p className="font-bold text-1xl text-slate-300 absolute w-full md:px-0 px-10 md:w-[50%] z-10 mt-10 ml-7">
